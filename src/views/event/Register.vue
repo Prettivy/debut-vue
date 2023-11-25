109065b3bd3ec05c95effd074832f0bc68d2ef78
AntoineRouichi <antoine.rouichi@gmail.com>
2023-11-25 22:18:45 +0100

    Ajout d'une propriété globale

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { inject } from 'vue';
 import { useRouter } from 'vue-router';
 const props = defineProps(["event"])
 const router = useRouter()
+const GStore = inject('GStore')
 
 const register = ()=>{
+    GStore.flashMessage = 'You are successfully registerd for' + event.title
+    setTimeout(()=>{
+        GStore.flashMessage = ''
+    }, 3000)
     router.push({
         name : 'event-details',
     })
