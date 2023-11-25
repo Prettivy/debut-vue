1ee2c0f4c1f3bce8a33e991c39d9249ff5e05a5d
AntoineRouichi <antoine.rouichi@gmail.com>
2023-11-25 21:48:15 +0100

    Redirection avec programmation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+import { useRouter } from 'vue-router';
 const props = defineProps(["event"])
+const router = useRouter()
+
+const register = ()=>{
+    router.push({
+        name : 'event-details',
+    })
+}
 </script>
 
 <template>
   <p>Register for the event here</p>
-
+    <button @click="register">Register Me</button>
 </template>
